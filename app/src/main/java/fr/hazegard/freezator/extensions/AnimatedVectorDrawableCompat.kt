7e962055c8a7dbbcbc538b5e26b3620c7d4f1b85
Hazegard <m.catrice@outlook.com>
2019-01-18 10:25:43 +0100

    remove old function

--- app/src/main/java/fr/hazegard/freezator/extensions/AnimatedVectorDrawableCompat.kt
+++ app/src/main/java/fr/hazegard/freezator/extensions/AnimatedVectorDrawableCompat.kt
@@ -7,16 +7,6 @@ import android.os.Looper
 import android.support.graphics.drawable.Animatable2Compat
 import android.support.graphics.drawable.AnimatedVectorDrawableCompat
 
-
-//fun Animatable.onAnimationEnd(callback: () -> Unit) {
-//    registerAnimationCallback(object : Animatable2Compat.AnimationCallback() {
-//        val handler = Handler(Looper.getMainLooper())
-//        override fun onAnimationEnd(drawable: Drawable?) {
-//            handler.post { callback() }
-//        }
-//    })
-//}
-
 fun Animatable.onAnimationEnd(callback: () -> Unit) {
     AnimatedVectorDrawableCompat.registerAnimationCallback(
             this as Drawable,
